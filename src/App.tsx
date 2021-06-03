b16eb3d6f3acce6a2271720efa5bd0315fb94879
kestutisvirbickas <v.kestutis123@gmail.com>
2021-06-03 14:33:55 +0300

    remove unneeded files

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const Hello = () => {
             <span role="img" aria-label="books">
               📚
             </span>
-            Read our docs
+            Read our docsąąą
           </button>
         </a>
         <a
